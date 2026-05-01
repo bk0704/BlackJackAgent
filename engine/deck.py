6231c60c70bd89b4e7c36e459fc5456b594895dd
bk0704 <bilal.k1@icloud.com>
2026-05-01 14:43:45 -0400

    Add Deck class with card shuffling and drawing functionality

--- engine/deck.py
+++ engine/deck.py
@@ -1,1 +1,16 @@
+import random
 
+CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 'A']
+
+class Deck:
+    def __init__(self):
+        self.cards = CARD_VALUES * 4
+        self.shuffle()
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        if not self.cards:
+            raise IndexError("Cannot draw from an empty deck")
+        return self.cards.pop()
