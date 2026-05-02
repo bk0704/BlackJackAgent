af84d65fc503c28183ba69deb3b59a57580d976f
bk0704 <bilal.k1@icloud.com>
2026-05-02 17:09:47 -0400

    Refactor Deck class to improve card initialization and add auto-reshuffle on low cards

--- engine/deck.py
+++ engine/deck.py
@@ -1,16 +1,16 @@
 import random
 
-CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 'A']
-
 class Deck:
     def __init__(self):
-        self.cards = CARD_VALUES * 4
-        self.shuffle()
-
+        numbered = list(range(2, 11))
+        face = ['J', 'Q', 'K']
+        ace = ['A']
+        self.cards = (numbered + face + ace) * 4
     def shuffle(self):
         random.shuffle(self.cards)
-
     def draw(self):
-        if not self.cards:
-            raise IndexError("Cannot draw from an empty deck")
+        if len(self.cards) < 15:
+            self.__init__()
+            self.shuffle()
         return self.cards.pop()
+    
